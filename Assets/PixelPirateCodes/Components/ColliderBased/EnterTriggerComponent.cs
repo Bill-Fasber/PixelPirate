34246a64a5a051009d3ac935f8b74c9f0cddbf6a
Bill-Fasber <86791256+Bill-Fasber@users.noreply.github.com>
2024-08-17 11:51:11 +0300

    0.1.11.3

--- Assets/PixelPirateCodes/Components/ColliderBased/EnterTriggerComponent.cs
+++ Assets/PixelPirateCodes/Components/ColliderBased/EnterTriggerComponent.cs
@@ -1,18 +1,20 @@
-﻿using UnityEngine;
+﻿using PixelPirateCodes.Utils;
+using UnityEngine;
 
-namespace PixelPirateCodes.Components
+namespace PixelPirateCodes.Components.ColliderBased
 {
     public class EnterTriggerComponent : MonoBehaviour
     {
         [SerializeField] private string _tag;
+        [SerializeField] private LayerMask _layer = ~0;
         [SerializeField] private EnterEvent _action;
 
         private void OnTriggerEnter2D(Collider2D other) 
         {
-            if(string.IsNullOrEmpty(_tag) || other.gameObject.CompareTag(_tag)) 
-            {
-                _action?.Invoke(other.gameObject);
-            }
+            if (!other.gameObject.IsInLayer(_layer)) return;
+            if(!string.IsNullOrEmpty(_tag) && !other.gameObject.CompareTag(_tag)) return;
+
+            _action?.Invoke(other.gameObject);
         }
     }
 }

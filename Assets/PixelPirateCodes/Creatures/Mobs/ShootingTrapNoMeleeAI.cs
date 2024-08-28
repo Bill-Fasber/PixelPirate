a212414f1b63e5b21ac392ba101cafc67f6f3a2f
Bill-Fasber <86791256+Bill-Fasber@users.noreply.github.com>
2024-08-28 21:18:55 +0300

    0.1.12.6

---
+++ Assets/PixelPirateCodes/Creatures/Mobs/ShootingTrapNoMeleeAI.cs
@@ -0,0 +1,47 @@
+﻿using PixelPirateCodes.Components.ColliderBased;
+using PixelPirateCodes.Components.GoBased;
+using PixelPirateCodes.Utils;
+using UnityEngine;
+
+namespace PixelPirateCodes.Creatures.Mobs
+{
+    public class ShootingTrapNoMeleeAI : MonoBehaviour
+    {
+        [SerializeField] private ColliderCheck _vision;
+
+        [Header("Range")]
+        [SerializeField] private Cooldown _rangeCooldown;
+        [SerializeField] private SpawnComponent _rangeAttack;
+        
+        private static readonly int Melee = Animator.StringToHash("melee");
+        private static readonly int Range = Animator.StringToHash("range");
+
+        
+        private Animator _animator;
+
+        private void Awake()
+        {
+            _animator = GetComponent<Animator>();
+        }
+
+        private void Update()
+        {
+            if (_vision.IsTouchingLayer)
+            {
+                if (_rangeCooldown.IsReady)
+                    RangeAttack();
+            }
+        }
+
+        private void RangeAttack()
+        {
+            _rangeCooldown.Reset();
+            _animator.SetTrigger(Range);
+        }
+
+        private void OnRangeAttack()
+        {
+            _rangeAttack.Spawn();
+        }
+    }
+}

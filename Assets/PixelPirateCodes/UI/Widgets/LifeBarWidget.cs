ba9077e15a72e5215050794307831afa88eaa7cf
Bill-Fasber <86791256+Bill-Fasber@users.noreply.github.com>
2025-07-18 07:13:04 +0300

    Revert "PixelPirateСW17"
    This reverts commit 4bf52007188a850d9c161f0cec7724667b5b40af, reversing
    changes made to 938be3fbea4038c86fd53094611e756ded5f4aab.

--- Assets/PixelPirateCodes/UI/Widgets/LifeBarWidget.cs
+++ Assets/PixelPirateCodes/UI/Widgets/LifeBarWidget.cs
@@ -11,10 +11,6 @@ namespace PixelPirateCodes.UI.Widgets
 
         private readonly CompositeDisposable _trash = new CompositeDisposable();
         private int _maxHp;
-<<<<<<< HEAD
-=======
-        
->>>>>>> c14b2051b1fccf81e70de703d6ce980bd4297d09
         private void Start()
         {
             if (_hp == null)
@@ -23,7 +19,6 @@ namespace PixelPirateCodes.UI.Widgets
             _maxHp = _hp.Health;
 
             _trash.Retain(_hp._onDie.Subscribe(OnDie));
-<<<<<<< HEAD
             _trash.Retain(_hp._onChange.Subscribe(OnSpChanged));
         }
 
@@ -33,17 +28,6 @@ namespace PixelPirateCodes.UI.Widgets
         }
 
         private void OnSpChanged(int hp)
-=======
-            _trash.Retain(_hp._onChange.Subscribe(OnHpChanged));
-        }
-
-        private void OnDie()
-        {
-            Destroy(gameObject);
-        }
-
-        private void OnHpChanged(int hp)
->>>>>>> c14b2051b1fccf81e70de703d6ce980bd4297d09
         {
             var progress = (float) hp / _maxHp;
             _lifeBar.SetProgress(progress);
